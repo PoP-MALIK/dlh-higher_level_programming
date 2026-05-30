262001243b68a92ee090900f78c17df0fef05354
PoP-MALIK <melek.jaffel@gmail.com>
2026-05-30 15:20:18 +0000

    2fix

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """Append a string to a text file and return the number of characters added.
+    """Append a string to a file and return the number of characters added.
 
     Args:
         filename (str): The name of the file to append to.
